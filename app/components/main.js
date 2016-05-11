8212a68db42a2719701e1c5efc70e816b9737da3
jdmorlan <jay.d.morlan@gmail.com>
2016-05-11 16:58:10 -0500

    Add eslint support

--- app/components/main.js
+++ app/components/main.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const Main = React.createClass({
-  render: function() {
-    return(
+  render () {
+    return (
       <div>
         <h1>Main</h1>
       </div>
     )
   }
-});
+})
 
 export default Main
